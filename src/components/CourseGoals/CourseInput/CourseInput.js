10ec95a8bf2c19cccbb069bc1c582f625f4eebda
martin-wachira <martinnwachira@gmail.com>
2021-06-25 15:06:33 +0300

    dynamically change styles based on the props passed

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -11,26 +11,23 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")};
   }
 
   & input {
     display: block;
     width: 100%;
-    border: 1px solid #ccc;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
+    background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
   }
 
   & input:focus {
     outline: none;
     background: #fad0ec;
     border-color: #8b005d;
-  }
-
-  &.invalid input {
-    border-color: rgb(230, 40, 40);
-    background-color: rgb(209, 126, 126);
   }
 
   &.invalid label {
@@ -60,7 +57,7 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={!isValid && "invalid"}>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
