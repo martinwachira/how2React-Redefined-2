385cfbc86f84e5996132cb6a8dfb1b7af356f5d7
martin-wachira <martinnwachira@gmail.com>
2021-06-25 21:12:14 +0300

    add user list comp

---
+++ src/components/UserList/UserList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserList = (props) => {
+  //   const onSaveHandler = (user) => {};
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default UserList;
