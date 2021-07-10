b6a9c805df8df2cc5c8829ccbcb747094a3cce38
martin-wachira <martinnwachira@gmail.com>
2021-07-10 17:03:03 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-<<<<<<< HEAD
-ReactDOM.render(<App />, document.getElementById('root'));
-=======
-ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> eb0732819782f9ff8db22e6ee0b308f1cdfcb18c
+ReactDOM.render(<App />, document.getElementById("root"));
