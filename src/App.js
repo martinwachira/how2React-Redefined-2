f8e818b2a65f5002c38b971db9de90480064ea31
martin-wachira <martinnwachira@gmail.com>
2021-07-15 15:43:11 +0300

    update context __eliminate use of props to auth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -27,13 +27,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
