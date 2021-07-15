2d48f9d46087a26931fd10698ff403c9b12ea6d6
martin-wachira <martinnwachira@gmail.com>
2021-07-15 17:46:25 +0300

    make context dynamic __in this case pass object and func in the context

--- src/App.js
+++ src/App.js
@@ -27,8 +27,10 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}
+    >
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -3,7 +3,7 @@ import AuthContext from "../../store/auth-context";
 
 import classes from "./Navigation.module.css";
 
-const Navigation = (props) => {
+const Navigation = () => {
   const ctx = useContext(AuthContext);
   return (
     <nav className={classes.nav}>
@@ -20,7 +20,7 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
