a3e80671d69325dbe5a1d394003b68a9fde9d5e9
martin-wachira <martinnwachira@gmail.com>
2021-07-18 00:08:59 +0300

    change page name ...emoji :D

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -6,7 +6,7 @@ import classes from "./MainHeader.module.css";
 const MainHeader = (props) => {
   return (
     <header className={classes["main-header"]}>
-      <h1>A Typical Page</h1>
+      <h1>A Page 😇</h1>
       <Navigation onLogout={props.onLogout} />
     </header>
   );
