cbb57b1d1d0ad53c0e31ed1e694158045e7ce75b
martin-wachira <martinnwachira@gmail.com>
2021-07-20 12:54:05 +0300

    init header cart button" "

---
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -0,0 +1,13 @@
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = () => {
+  return (
+    <button className={classes.button}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
