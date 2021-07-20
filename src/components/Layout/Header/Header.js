581f67d16adc000290f8ff55aa690edf9e5458a6
martin-wachira <martinnwachira@gmail.com>
2021-07-20 12:59:05 +0300

    update header cart button with cool icon and style

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import mealsImg from "../../../assets/meals.jpg";
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>How2-React Meals App 🍝</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImg} alt="some delicious food" />
--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,11 +1,14 @@
 import classes from "./HeaderCartButton.module.css";
+import CartIcon from "../../Cart/CartIcon";
 
 const HeaderCartButton = () => {
   return (
     <button className={classes.button}>
-      <span></span>
-      <span></span>
-      <span></span>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Order</span>
+      <span className={classes.badge}>5</span>
     </button>
   );
 };
