ea333f1fa45ab86fbb30fb3862fa8e2583b8b266
martin-wachira <martinnwachira@gmail.com>
2021-07-20 12:16:10 +0300

    prep food order app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import React, { useContext } from "react";
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
-import AuthContext from "./store/auth-context";
-
 function App() {
-  const ctx = useContext(AuthContext);
   return (
-    <>
-      <MainHeader />
-      <main>
-        {!ctx.isLoggedIn && <Login />}
-        {ctx.isLoggedIn && <Home />}
-      </main>
-    </>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 
-import "./index.css";
-import App from "./App";
-import { AuthContextProvider } from "./store/auth-context";
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
-  <AuthContextProvider>
-    <App />
-  </AuthContextProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
