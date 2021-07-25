26a9a8d8736acf3ece6b4c52638bcabdd807c52c
martin-wachira <martinnwachira@gmail.com>
2021-07-25 14:09:54 +0300

    forwarded all props related to an input in parent input comp

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -4,7 +4,7 @@ const Input = (props) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input />
+      <input {...props.input} />
     </div>
   );
 };
