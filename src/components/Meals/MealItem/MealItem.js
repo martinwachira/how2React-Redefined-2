0b2f6100bce6f6b3e40494b089ac1af41ae73768
martin-wachira <martinnwachira@gmail.com>
2021-07-25 14:07:26 +0300

    intro mealitemform comp ...init new HOC input comp

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
@@ -9,6 +10,9 @@ const MealItem = (props) => {
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm />
       </div>
     </li>
   );
---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,21 @@
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          defaultValue: "1",
+          min: "1",
+          max: "5",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
