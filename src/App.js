ba696e859e8e4272fd85d394ac4b13aff683763a
martin-wachira <martinnwachira@gmail.com>
2021-08-04 21:05:45 +0300

    replace fragment with CartProvider comp in Appjs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [showCart, setShowCart] = useState(false);
@@ -15,13 +16,13 @@ function App() {
   };
 
   return (
-    <>
+    <CartProvider>
       {showCart && <Cart onCloseCart={handleCloseCart} />}
       <Header onOpenCart={handleOpenCart} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
