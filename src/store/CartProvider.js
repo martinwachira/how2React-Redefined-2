3f055509d7df0b269464f4bff309ceaae831ef6c
martin-wachira <martinnwachira@gmail.com>
2021-08-04 20:49:22 +0300

    update context provider, defined handler functions

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,6 +1,14 @@
 import CartContext from "./cart-contex";
 
 const CartProvider = (props) => {
+  const addItemToCartHandler = (item) => {};
+  const removeItemFromCartHandler = (id) => {};
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
   return <CartContext.Provider>{props.children}</CartContext.Provider>;
 };
 
