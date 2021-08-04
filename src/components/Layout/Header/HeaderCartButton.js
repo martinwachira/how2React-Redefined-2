e5586b2cd9545da8179785aa36f618a27386793a
martin-wachira <martinnwachira@gmail.com>
2021-08-04 22:08:22 +0300

    use reduce method to get the list of item numbers'

--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,14 +1,21 @@
+import { useContext } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../../Cart/CartIcon";
+import CartContext from "../../../store/cart-contex";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+  console.log("cartCtx", cartCtx);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Order</span>
-      <span className={classes.badge}>5</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
