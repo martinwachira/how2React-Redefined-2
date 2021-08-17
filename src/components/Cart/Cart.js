e16f9e8c5b421f66a1c7b25156242c0f33271c08
martin-wachira <martinnwachira@gmail.com>
2021-08-17 14:27:27 +0300

    implement removeitem from cart handler ...updated reducer remove cart function tobe able to decrease the items and their prices in the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,7 +8,9 @@ const Cart = (props) => {
   const cartCtx = useContext(CartContext);
   const totalAmount = `$ ${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({ ...item, amount: 1 });
   };
