4e146d88b79579b4e0db12dc931b9f6d39a4df73
martin-wachira <martinnwachira@gmail.com>
2021-08-17 14:45:59 +0300

    useEffect hook to animate cart button on item add

--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,16 +1,28 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../../Cart/CartIcon";
 import CartContext from "../../../store/cart-contex";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
-  console.log("cartCtx", cartCtx);
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+  }, [items]);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
