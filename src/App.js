7787e68b4fb5506b2edfb6f4a48f8f21a1ac07d4
martin-wachira <martinnwachira@gmail.com>
2021-08-18 11:28:31 +0300

    init paragraph toggle ...to see how react updates state and DOM

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
 
 import "./App.css";
 
 function App() {
+  const [toggleP, setToggleP] = useState(false);
+  const togglePHandler = () => {
+    // setToggleP(!toggleP);
+    setToggleP((previousShowP) => !previousShowP);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {toggleP && <p>Thee paragraph</p>}
+      <Button onClick={togglePHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
