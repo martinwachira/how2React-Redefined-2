474bff3ed82a2000b6fc08c602bc0ca2e91709b9
martin-wachira <martinnwachira@gmail.com>
2021-08-18 11:52:40 +0300

    track comp re-evaluation on child comp

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { useState } from "react";
 import Button from "./components/UI/Button/Button";
 
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
-  const [toggleP, setToggleP] = useState(false);
+  const [showParag, setShowParag] = useState(false);
   const togglePHandler = () => {
     // setToggleP(!toggleP);
-    setToggleP((previousShowP) => !previousShowP);
+    setShowParag((previousShowP) => !previousShowP);
   };
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {toggleP && <p>Thee paragraph</p>}
+      <DemoOutput show={showParag} />
       <Button onClick={togglePHandler}>Toggle Paragraph</Button>
     </div>
   );
--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -1,5 +1,7 @@
 import React from "react";
 
-const DemoOutput = (props) => {};
+const DemoOutput = (props) => {
+  return <p>{props.show ? "Thee paragraph" : ""}</p>;
+};
 
 export default DemoOutput;
