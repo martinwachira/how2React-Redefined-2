cbd65a14fd975a14f893669cfdd06a5d9844e7c5
martin-wachira <martinnwachira@gmail.com>
2021-08-24 15:07:19 +0300

    intro class based comps

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import React, { useState, useCallback, useMemo } from "react";
-import Button from "./components/UI/Button/Button";
-import DemoList from "./components/Demo/DemoList";
-
-import "./App.css";
+import Users from './components/Users';
 
 function App() {
-  const [listTitle, setListTitle] = useState("My List");
-
-  const titleChangeHandler = useCallback(() => {
-    setListTitle("New Title");
-  }, []);
-
-  const listItems = useMemo(() => [5, 3, 2, 9, 1], []);
-
   return (
-    <div className="app">
-      <DemoList title={listTitle} items={listItems} />
-      <Button onClick={titleChangeHandler}>Change List Title</Button>
+    <div>
+      <Users />
     </div>
   );
 }
