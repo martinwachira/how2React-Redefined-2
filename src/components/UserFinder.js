25a1f6abd7cb40c8bfc046af9cbe74b87e1b9f88
martin-wachira <martinnwachira@gmail.com>
2021-08-26 19:47:36 +0300

    use context in class based comps ...only one context can be used in a comp when defining context via contextType

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,14 +1,11 @@
 import { Fragment, Component } from "react";
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
-
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
+import UsersContext from "../store/users-context";
 
 class UserFinder extends Component {
+  //context definition
+  static contextType = UsersContext;
   constructor() {
     super();
     this.state = {
@@ -18,14 +15,17 @@ class UserFinder extends Component {
   }
 
   componentDidMount() {
-    this.setState({ filteredUsers: DUMMY_USERS });
+    console.log("the context", this.context);
+
+    //map filteredUsers state to the users context
+    this.setState({ filteredUsers: this.context.users });
   }
 
   //replicate useEffect hook in functional comp
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.toLowerCase().includes(this.state.searchTerm.toLowerCase())
         ),
       });
@@ -46,31 +46,5 @@ class UserFinder extends Component {
     );
   }
 }
-
-// const UserFinder = () => {
-//   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
-//   const [searchTerm, setSearchTerm] = useState("");
-
-//   useEffect(() => {
-//     setFilteredUsers(
-//       DUMMY_USERS.filter((user) =>
-//         user.name.toLowerCase().includes(searchTerm.toLowerCase())
-//       )
-//     );
-//   }, [searchTerm]);
-
-//   const searchChangeHandler = (event) => {
-//     setSearchTerm(event.target.value);
-//   };
-
-//   return (
-//     <Fragment>
-//       <div className={classes.finder}>
-//         <input type="search" onChange={searchChangeHandler} />
-//       </div>
-//       <Users users={filteredUsers} />
-//     </Fragment>
-//   );
-// };
 
 export default UserFinder;
