3c30b30dc79df5f2868ec206418095bd991270b4
martin-wachira <martinnwachira@gmail.com>
2021-08-31 11:05:57 +0300

    sending a GET Req ...use open api  to fetch movies from the api

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import Movie from './Movie';
-import classes from './MoviesList.module.css';
+import Movie from "./Movie";
+import classes from "./MoviesList.module.css";
 
 const MovieList = (props) => {
   return (
-    <ul className={classes['movies-list']}>
+    <ul className={classes["movies-list"]}>
       {props.movies.map((movie) => (
         <Movie
           key={movie.id}
