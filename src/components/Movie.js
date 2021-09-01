a81b9b31c70ff4dce05e5d389ccf1a97c70adee6
martinwachira <martinnwachira@gmail.com>
2021-09-01 15:04:46 +0300

    update: restyle

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
+  
   return (
     <li className={classes.movie}>
       <h2>{props.title}</h2>
