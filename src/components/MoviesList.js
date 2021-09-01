42de9a61fac0fe7bc354116d2a699a801eca015a
martinwachira <martinnwachira@gmail.com>
2021-09-01 15:14:03 +0300

    update: restyle

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,11 +1,10 @@
-import React from "react";
-
-import Movie from "./Movie";
-import classes from "./MoviesList.module.css";
+import Movie from './Movie';
+import React from 'react';
+import classes from './MoviesList.module.css';
 
 const MovieList = (props) => {
   return (
-    <ul className={classes["movies-list"]}>
+    <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
         <Movie
           key={movie.id}
