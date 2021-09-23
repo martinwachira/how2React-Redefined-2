cd21572b9ec5294b39721f5dea591a8d10fcfb52
martinwachira <martinnwachira@gmail.com>
2021-09-23 18:26:36 +0300

    update: create an add movie method - use realtime firebase db

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import './App.css';
+import "./App.css";
 
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from "react";
 
-import AddMovie from './components/AddMovie';
-import MoviesList from './components/MoviesList';
+import AddMovie from "./components/AddMovie";
+import MoviesList from "./components/MoviesList";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch(
+        "https://react-redefined-default-rtdb.firebaseio.com/movies.json"
+      );
       if (!response.ok) {
-        throw new Error('Something went wrong!');
+        throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
@@ -42,7 +43,14 @@ function App() {
   }, [fetchMoviesHandler]);
 
   function addMovieHandler(movie) {
-    console.log(movie);
+    // console.log(movie);
+    fetch("https://react-redefined-default-rtdb.firebaseio.com/movies.json", {
+      method: "POST",
+      body: JSON.stringify(movie),
+      headers: {
+        "Content-Type": "Application/json",
+      },
+    });
   }
 
   let content = <p>Found no movies.</p>;
