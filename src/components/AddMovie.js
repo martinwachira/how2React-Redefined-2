2cbdad36e73c1a65dc796dc0447ab5cc19263212
martinwachira <martinnwachira@gmail.com>
2021-09-28 11:01:54 +0300

    --

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,12 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 
-import classes from './AddMovie.module.css';
+import classes from "./AddMovie.module.css";
 
 function AddMovie(props) {
-  
-  const titleRef = useRef('');
-  const openingTextRef = useRef('');
-  const releaseDateRef = useRef('');
+  const titleRef = useRef("");
+  const openingTextRef = useRef("");
+  const releaseDateRef = useRef("");
 
   function submitHandler(event) {
     event.preventDefault();
@@ -25,16 +24,16 @@ function AddMovie(props) {
   return (
     <form onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='title'>Title</label>
-        <input type='text' id='title' ref={titleRef} />
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" ref={titleRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor='opening-text'>Opening Text</label>
-        <textarea rows='5' id='opening-text' ref={openingTextRef}></textarea>
+        <label htmlFor="opening-text">Opening Text</label>
+        <textarea rows="5" id="opening-text" ref={openingTextRef}></textarea>
       </div>
       <div className={classes.control}>
-        <label htmlFor='date'>Release Date</label>
-        <input type='text' id='date' ref={releaseDateRef} />
+        <label htmlFor="date">Release Date</label>
+        <input type="text" id="date" ref={releaseDateRef} />
       </div>
       <button>Add Movie</button>
     </form>
