4bd93a9a382f6533a75e6dba5ad16c26de6c445a
martinwachira <martinnwachira@gmail.com>
2021-09-28 14:30:27 +0300

    init custom hook ...useHttp hook, assist reuse send api req functionality

---
+++ src/hooks/use-http.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const useHttp = (reqConfig, applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(reqConfig.url, {
+        method: reqConfig.method,
+        headers: reqConfig.headers,
+        body: JSON.stringify(reqConfig.body),
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
