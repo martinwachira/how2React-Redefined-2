f72b81bdbfd67d2cefa13bac6941acff6568537a
martinwachira <martinnwachira@gmail.com>
2021-09-29 13:00:08 +0300

    init state to manipulate entered input value

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 const SimpleInput = (props) => {
+  const [name, setName] = useState("");
+
+  const nameInputHandler = (e) => {
+    setName(e.target.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    console.log("Entered name:", name);
+    setName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={onSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" value={name} onChange={nameInputHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
