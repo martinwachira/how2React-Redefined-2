fc7d48420fbbb55e690d1cb9c688fd0e7cbacb90
martinwachira <martinnwachira@gmail.com>
2021-09-30 19:13:56 +0300

    validate form inputs

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 const SimpleInput = (props) => {
   const [name, setName] = useState("");
+  const [nameIsValid, setNameIsValid] = useState(true);
 
   const nameInputHandler = (e) => {
     setName(e.target.value);
@@ -9,21 +10,26 @@ const SimpleInput = (props) => {
   const onSubmitHandler = (e) => {
     e.preventDefault();
 
-    if (name.trim("") === "") {
-      alert("no value");
+    if (name.trim() === "") {
+      setNameIsValid(false);
       return;
     }
+
+    setNameIsValid(true);
 
     console.log("Entered name:", name);
     setName("");
   };
 
+  const inputClasses = nameIsValid ? "form-control" : "form-control invalid";
+
   return (
     <form onSubmit={onSubmitHandler}>
-      <div className="form-control">
+      <div className={inputClasses}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" value={name} onChange={nameInputHandler} />
       </div>
+      {!nameIsValid && <p className="error-text">Name Mustn't be Empty</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
