82449bd00a221cf3fbd071db10db43aa2284d1e5
martinwachira <martinnwachira@gmail.com>
2021-10-07 11:48:24 +0300

    init: custom useinput hook

---
+++ src/hooks/use-input.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const useInput = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHandler = (e) => {
+    setEnteredValue(e.target.value);
+  };
+
+  const inputBlurHandler = (e) => {
+    setIsTouched(true);
+  };
+
+  return {
+    value: enteredValue,
+    valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+  };
+};
+
+export default useInput;
