e6365481e501add2ea2314dfcdecc0c08fb020b1
martinwachira <martinnwachira@gmail.com>
2021-10-13 14:09:17 +0300

    use basic form

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,21 +1,29 @@
+// import customHook from "../hooks/custom-hook";
+
 const BasicForm = (props) => {
+  const fname = "";
+  const lname = "";
+  const address = "";
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="control-group">
+        <div className="form-control">
+          <label htmlFor="name">First Name</label>
+          <input type="text" id="name" name={fname} />
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" name={lname} />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="text" id="name" name={address} />
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
