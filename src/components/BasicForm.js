3cb6321eb7ed9a10ae6b3560088822cb58dab5b7
martinwachira <martinnwachira@gmail.com>
2021-10-13 15:37:57 +0300

    validate form and use custom hook

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,37 +1,57 @@
 import useCustom from "./../hooks/custom-hook";
 
-const BasicForm = (props) => {
+const BasicForm = () => {
   const {
     value: fname,
+    inputHasError: fNameError,
+    valueIsValid: fNameIsValid,
     resetInput: resetFname,
     inputChangeHandler: fNameChangeHandler,
     inputBlurHandler: fNameBlurHandler,
-  } = useCustom();
+  } = useCustom((value) => value.trim("") !== "");
   const {
     value: lname,
+    inputHasError: lNameError,
+    valueIsValid: lNameIsValid,
     resetInput: resetLname,
     inputChangeHandler: lNameChangeHandler,
     inputBlurHandler: lNameBlurHandler,
-  } = useCustom();
+  } = useCustom((value) => value.trim("") !== "");
   const {
     value: address,
+    inputHasError: addressError,
+    valueIsValid: addressIsValid,
     resetInput: resetAddress,
     inputChangeHandler: addressChangeHandler,
     inputBlurHandler: addressBlurHandler,
-  } = useCustom();
+  } = useCustom((value) => value.includes("@") && value.includes("."));
+
+  let formIsValid = false;
+
+  if (fNameIsValid && lNameIsValid && addressIsValid) {
+    formIsValid = true;
+  }
 
   const submitHandler = (e) => {
     e.preventDefault();
+
+    if (!fNameIsValid && !lNameIsValid) {
+      return;
+    }
 
     resetFname("");
     resetLname("");
     resetAddress("");
   };
 
+  const fNameClasses = fNameError ? "form-control invalid" : "form-control";
+  const lNameClasses = lNameError ? "form-control invalid" : "form-control";
+  const addressClasses = addressError ? "form-control invalid" : "form-control";
+
   return (
     <form onSubmit={submitHandler}>
       <div className="control-group">
-        <div className="form-control">
+        <div className={fNameClasses}>
           <label htmlFor="name">First Name</label>
           <input
             type="text"
@@ -40,8 +60,11 @@ const BasicForm = (props) => {
             onChange={fNameChangeHandler}
             onBlur={fNameBlurHandler}
           />
+          {fNameError && (
+            <p className="error-text">First Name Mustn't be Empty</p>
+          )}
         </div>
-        <div className="form-control">
+        <div className={lNameClasses}>
           <label htmlFor="name">Last Name</label>
           <input
             type="text"
@@ -50,9 +73,12 @@ const BasicForm = (props) => {
             onChange={lNameChangeHandler}
             onBlur={lNameBlurHandler}
           />
+          {lNameError && (
+            <p className="error-text">Last Name Mustn't be Empty</p>
+          )}
         </div>
       </div>
-      <div className="form-control">
+      <div className={addressClasses}>
         <label htmlFor="name">E-Mail Address</label>
         <input
           type="text"
@@ -61,9 +87,10 @@ const BasicForm = (props) => {
           onChange={addressChangeHandler}
           onBlur={addressBlurHandler}
         />
+        {addressError && <p className="error-text">Wrong email Format</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
