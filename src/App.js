a5fb2a5dc9fb35a8273a4bc86140d50017a7b5c0
martinwachira <martinnwachira@gmail.com>
2021-10-14 19:00:20 +0300

    init test form to store data in db

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 import Header from "./components/Layout/Header/Header";
 import Meals from "./components/Meals/Meals";
-import CartProvider from "./store/CartProvider";
 
 function App() {
   const [showCart, setShowCart] = useState(false);
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,10 +1,13 @@
-import React from "react";
+import AddMeal from "./AddMeal";
+// import AddMeal from "./AddMeal";
 import AvailableMeals from "./AvailableMeals";
 import MealsSummary from "./MealsSummary";
+import React from "react";
 
 const Meals = () => {
   return (
     <>
+      <AddMeal />
       <MealsSummary />
       <AvailableMeals />
     </>
