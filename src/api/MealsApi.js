81a62d6444b50716232cddda454536ccecd31fed
martinwachira <martinnwachira@gmail.com>
2021-10-18 10:18:28 +0300

    ...init new api file for meals post/get

---
+++ src/api/MealsApi.js
@@ -0,0 +1,16 @@
+export async function postMeals(meals) {
+  const res = await fetch(
+    "https://react-redefined-default-rtdb.firebaseio.com/meals.json",
+    {
+      method: "POST",
+      body: JSON.stringify(meals),
+      headers: {
+        "Content-Type": "Application/json",
+      },
+    }
+  );
+  const data = await res.json();
+  if (res.status === 200) {
+    console.log("data", data);
+  }
+}
