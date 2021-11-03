a4b0a9245a0a4c91f9fa125390e1409a22e46eaf
martinwachira <martinnwachira@gmail.com>
2021-11-03 17:20:51 +0300

    checkout form

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,7 +1,27 @@
 import classes from "./Checkout.module.css";
 
 const Checkout = (props) => {
-  return <form className={classes.form}></form>;
+  return (
+    <form>
+      <div className={classes.control}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" placeholder="name" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="name">Street</label>
+        <input type="text" id="street" placeholder="street" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" placeholder="postal" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="phone">Phone</label>
+        <input type="text" id="phone" placeholder="phone" />
+      </div>
+      <button>Confirm Order</button>
+    </form>
+  );
 };
 
 export default Checkout;
