05228b22de24fb28563ffa40d7fddd4a92276513
martinwachira <martinnwachira@gmail.com>
2022-03-04 16:49:25 +0300

    --init react router

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-import React, { useState } from "react";
-
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
-import Header from "./components/Layout/Header/Header";
-import Meals from "./components/Meals/Meals";
-
 function App() {
-  const [showCart, setShowCart] = useState(false);
-
-  const handleOpenCart = () => {
-    setShowCart(true);
-  };
-
-  const handleCloseCart = () => {
-    setShowCart(false);
-  };
-
   return (
-    <CartProvider>
-      {showCart && <Cart onCloseCart={handleCloseCart} />}
-      <Header onOpenCart={handleOpenCart} />
-      <main>
-        <Meals />
-      </main>
-    </CartProvider>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
