c356e3155cbbec7be2cc1b4e4898b9780268b40e
martinwachira <martinnwachira@gmail.com>
2022-03-14 13:40:30 +0300

    update pkgs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Button from "./Components/Common/Button.js";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Button label="Click Me"></Button>
     </div>
   );
 }
