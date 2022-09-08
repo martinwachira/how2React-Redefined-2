274e0862280c6bfb03d08701ccf02661f57c45cf
martinwachira <martinnwachira@gmail.com>
2022-09-09 02:41:08 +0300

    sneakyclear

---
+++ src/Components/MainHeader.js
@@ -0,0 +1,18 @@
+const MainHeader = () => {
+  return (
+    <>
+      <header>
+        <ul>
+          <li>
+            <a href="/welcome">Welcome</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+};
+
+export default MainHeader;
