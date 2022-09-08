3e64dec4e1707aeaecc31aa49472e38a675f63c8
martinwachira <martinnwachira@gmail.com>
2022-09-09 02:38:38 +0300

    links -sneaky impl

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Router } from "react-router-dom";
 
 import About from "./Pages/About";
 import Button from "./Components/Common/Button.js";
+import MainHeader from "./Components/MainHeader";
 import Welcome from "./Pages/Welcome";
 
 function App() {
   return (
     <div>
       <Button label="Register"></Button>
+      <MainHeader />
       <Route path="/welcome">
         <Welcome />
       </Route>
