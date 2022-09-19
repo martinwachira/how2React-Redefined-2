16613e6ecf66bd8948c2468a60cfd61b9c1c479f
martinwachira <martinnwachira@gmail.com>
2022-09-19 16:57:33 +0300

    switch/exact routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
+import { Route, Switch } from "react-router-dom";
+
 import About from "./Pages/About";
 import Button from "./Components/Common/Button.js";
 import MainHeader from "./Components/MainHeader";
 import Product from "./Pages/Product";
 import ProductDetails from "./Pages/ProductDetails";
-import { Route } from "react-router-dom";
 import Welcome from "./Pages/Welcome";
 
 function App() {
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       {/* <Button label="Register"></Button> */}
       <MainHeader />
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/products">
-        <Product />
-      </Route>
-      <Route path="/product-details/:productId">
-        <ProductDetails />
-      </Route>
+      <Switch>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/products" exact>
+          <Product />
+        </Route>
+        <Route path="/products/:productId">
+          <ProductDetails />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
 const Product = () => {
   return (
     <>
-      <ul>Product 1</ul>
-      <ul>Product 2</ul>
-      <ul>Product 3</ul>
+      <ul>
+        <Link to="/products/p1">Product 1</Link>
+      </ul>
+      <ul>
+        <Link to="/products/p2">Product 2</Link>
+      </ul>
+      <ul>
+        <Link to="/products/p3">Product 3</Link>
+      </ul>
     </>
   );
 };
