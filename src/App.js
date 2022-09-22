297aecb786dd59ada7b6f9a5214557ecf5b382e9
martinwachira <martinnwachira@gmail.com>
2022-09-23 02:10:50 +0300

    main nav

--- src/App.js
+++ src/App.js
@@ -7,20 +7,22 @@ import React from "react";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="quotes" />
-      </Route>
-      <Route path="/quotes" exact>
-        <AllQuotes />
-      </Route>
-      <Route path="/quotes/:quoteId">
-        <QuoteDetail />
-      </Route>
-      <Route path="/add-quote">
-        <NewQuote />
-      </Route>
-    </Switch>
+    <>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/add-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
