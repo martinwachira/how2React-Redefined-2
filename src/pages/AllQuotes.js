9ee3180f69b346f8e08013dcce8f22c3460b5309
martinwachira <martinnwachira@gmail.com>
2022-09-24 22:29:03 +0300

    impl all quotes with dummy data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,10 +1,14 @@
-// import QuoteList from "../components/quotes/QuoteList";
-
+import QuoteList from "../components/quotes/QuoteList";
+const DUMMY_LIST = [
+  { id: "q1", author: "wcr", text: "That`s a mistake" },
+  { id: "q2", author: "writer", text: "This is fun" },
+  { id: "q3", author: "author 1", text: "Soar great" },
+];
 const AllQuotes = () => {
   return (
     <div>
-      <p>All Quotes Page</p>
-      {/* <QuoteList /> */}
+      {/* <p>All Quotes Page</p> */}
+      <QuoteList quotes={DUMMY_LIST} />
     </div>
   );
 };
