ee2b45fccb3713e100139b59e1ff50548f1a00a9
martinwachira <martinnwachira@gmail.com>
2022-10-04 19:41:29 +0300

    programmatic nav - history

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,7 +1,11 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
+
 const NewQuote = () => {
+  const history = useHistory();
   const addQuoteHandler = (quoteData) => {
     console.log("quoteData", quoteData);
+    history.push("/quotes");
   };
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
