fa4a2b7c28f93e088f3b67d7615c52770ae83ba6
martinwachira <martinnwachira@gmail.com>
2022-10-14 13:25:10 +0300

    impl query params

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,10 +1,22 @@
+import { useHistory, useLocation } from "react-router-dom";
+
 import { Fragment } from "react";
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+  console.log(location);
+  const changeSortingHandler = () => {
+    history.push("?sort=asc");
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort Ascending</button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
