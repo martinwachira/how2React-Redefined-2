b05d39e363a8a9279b5e07c9e90ea5ded288cc89
martinwachira <martinnwachira@gmail.com>
2022-10-17 15:01:37 +0300

    create quotes via http -> fb

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,13 +1,24 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import { addQuote } from "../lib/api";
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import useHttp from "../hooks/use-http";
 
 const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
+
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
   const addQuoteHandler = (quoteData) => {
-    console.log("quoteData", quoteData);
-    history.push("/quotes");
+    sendRequest(quoteData);
   };
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
 };
 
 export default NewQuote;
